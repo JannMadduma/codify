2bc78b086ef145926d30b090ed236897fc4e43e5
SselJay <russeljaybuan@gmail.com>
2022-10-17 20:40:40 +0800

    Create AboutUs.js

---
+++ src/components/Home/about/AboutUs.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ResponsiveAppBar from "../../common/Header";
+
+export default function AboutUs(){
+    return(
+        <>
+        <ResponsiveAppBar/>
+        </>
+    )
+}
