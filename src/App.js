987ff55cb15fffffdca455929016d6359647f938
Jann Fraulien Madduma <madduma.jannfraulien@gmail.com>
2022-10-18 17:52:45 +0800

    routed to home.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
-import Footer from "./components/common/Footer";
-import Header from "./components/common/Header";
-import Framework from "./components/Home/heropage/Framework";
-import PricingService from "./components/Home/pricingservices/PricingService";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <PricingService />
-      <Footer />
+      <Home />
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,9 @@ import Framework from "./heropage/Framework";
 import Technology from "./heropage/Technology";
 import Portfolio from "../common/Portfolio";
 import Footer from "../common/Footer";
-import Pricing from "../common/Pricing";
 import Feedbacks from "../common/Feedbacks";
 import CallToActions from "./heropage/CallToAction";
+import PricingService from "./pricingservices/PricingService";
 
 const Home = ({}) => {
   return (
@@ -17,7 +17,7 @@ const Home = ({}) => {
       <Framework />
       <Technology />
       <Portfolio />
-      <Pricing />
+      <PricingService />
       <Feedbacks />
       <CallToActions />
       <Footer />
