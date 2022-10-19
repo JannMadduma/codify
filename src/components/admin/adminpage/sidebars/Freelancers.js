97a0afa05d4e08ff47b2371cdfb31e776509e047
Ton Salingua <110211961+TonSalingua@users.noreply.github.com>
2022-10-19 11:43:46 +0800

    Prtfolio changes and Admin

---
+++ src/components/admin/adminpage/sidebars/Freelancers.js
@@ -0,0 +1,85 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import EditIcon from "@mui/icons-material/Edit";
+
+const styles = (theme) => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 700,
+  },
+});
+
+let id = 0;
+function createData(name, Name, Email, Status, Actions) {
+  id += 1;
+  return { id, name, Name, Email, Status, Actions };
+}
+
+const rows = [
+  createData(1, "Ton Salingua", "ton@gmail.com", "Active"),
+  createData(2, "Jann Frauline", "jann@gmail.com", "Active"),
+  createData(3, "Russel Moon", "russel@gmail.com", "Active"),
+];
+
+function SimpleTable(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell align="center">Name</TableCell>
+            <TableCell align="center">Email</TableCell>
+            <TableCell align="center">Status</TableCell>
+            <TableCell align="center">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="center">{row.Name}</TableCell>
+              <TableCell align="center">{row.Email}</TableCell>
+              <TableCell align="center">{row.Status}</TableCell>
+              <TableCell align="center">
+                <Button>
+                  <VisibilityIcon />
+                </Button>
+                <Button>
+                  <EditIcon />
+                </Button>
+                <Button>
+                  <DeleteIcon />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+SimpleTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTable);
--- src/components/common/Portfolio.js
+++ src/components/common/Portfolio.js
@@ -1,13 +1,13 @@
 import * as React from "react";
-import AspectRatio from "@mui/joy/AspectRatio";
 import Box from "@mui/material/Box";
-import Card from "@mui/joy/Card";
-import CardCover from "@mui/joy/CardCover";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/joy/Link";
 import { Grid } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import JannProjects from "../Home/portfolio/websites/JannProjects";
+import PhillipProjects from "../Home/portfolio/websites/PhillipProjects";
+import RechProjects from "../Home/portfolio/websites/RechProjects";
+import RusselProjects from "../Home/portfolio/websites/RusselProjects";
+import TonProjects from "../Home/portfolio/websites/TonProjects";
 
 const Item = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -27,203 +27,22 @@ function FormRow() {
       </Grid>
       <Grid item xs={3}>
         <Item>
-          <Card
-            sx={{
-              width: 300,
-              bgcolor: "initial",
-              boxShadow: "none",
-              "--Card-padding": "0px",
-            }}
-          >
-            <Box sx={{ position: "relative" }}>
-              <AspectRatio ratio="4/3">
-                <figure>
-                  <img
-                    src="https://images.unsplash.com/photo-1515825838458-f2a94b20105a?auto=format&fit=crop&w=300"
-                    srcSet="https://images.unsplash.com/photo-1515825838458-f2a94b20105a?auto=format&fit=crop&w=300&dpr=2 2x"
-                    loading="lazy"
-                    alt="Yosemite by Casey Horner"
-                  />
-                </figure>
-              </AspectRatio>
-              <CardCover
-                className="gradient-cover"
-                sx={{
-                  "&:hover, &:focus-within": {
-                    opacity: 1,
-                  },
-                  opacity: 0,
-                  transition: "0.1s ease-in",
-                  background:
-                    "linear-gradient(180deg, transparent 62%, rgba(0,0,0,0.00345888) 63.94%, rgba(0,0,0,0.014204) 65.89%, rgba(0,0,0,0.0326639) 67.83%, rgba(0,0,0,0.0589645) 69.78%, rgba(0,0,0,0.0927099) 71.72%, rgba(0,0,0,0.132754) 73.67%, rgba(0,0,0,0.177076) 75.61%, rgba(0,0,0,0.222924) 77.56%, rgba(0,0,0,0.267246) 79.5%, rgba(0,0,0,0.30729) 81.44%, rgba(0,0,0,0.341035) 83.39%, rgba(0,0,0,0.367336) 85.33%, rgba(0,0,0,0.385796) 87.28%, rgba(0,0,0,0.396541) 89.22%, rgba(0,0,0,0.4) 91.17%)",
-                }}
-              >
-                {/* The first box acts as a container that inherits style from the CardCover */}
-                <Box>
-                  <Box
-                    sx={{
-                      p: 2,
-                      display: "flex",
-                      alignItems: "center",
-                      gap: 1.5,
-                      flexGrow: 1,
-                      alignSelf: "flex-end",
-                    }}
-                  >
-                    <Typography level="h2" noWrap sx={{ fontSize: "lg" }}>
-                      <Link
-                        href="#dribbble-shot"
-                        overlay
-                        underline="none"
-                        sx={{
-                          color: "#fff",
-                          textOverflow: "ellipsis",
-                          overflow: "hidden",
-                          display: "block",
-                        }}
-                      >
-                        Yosemite
-                      </Link>
-                    </Typography>
-                  </Box>
-                </Box>
-              </CardCover>
-            </Box>
-          </Card>
+          <PhillipProjects />
         </Item>
       </Grid>
       <Grid item xs={3}>
         <Item>
-          <Card
-            sx={{
-              width: 300,
-              bgcolor: "initial",
-              boxShadow: "none",
-              "--Card-padding": "0px",
-            }}
-          >
-            <Box sx={{ position: "relative" }}>
-              <AspectRatio ratio="4/3">
-                <figure>
-                  <img
-                    src="https://images.unsplash.com/photo-1515825838458-f2a94b20105a?auto=format&fit=crop&w=300"
-                    srcSet="https://images.unsplash.com/photo-1515825838458-f2a94b20105a?auto=format&fit=crop&w=300&dpr=2 2x"
-                    loading="lazy"
-                    alt="Yosemite by Casey Horner"
-                  />
-                </figure>
-              </AspectRatio>
-              <CardCover
-                className="gradient-cover"
-                sx={{
-                  "&:hover, &:focus-within": {
-                    opacity: 1,
-                  },
-                  opacity: 0,
-                  transition: "0.1s ease-in",
-                  background:
-                    "linear-gradient(180deg, transparent 62%, rgba(0,0,0,0.00345888) 63.94%, rgba(0,0,0,0.014204) 65.89%, rgba(0,0,0,0.0326639) 67.83%, rgba(0,0,0,0.0589645) 69.78%, rgba(0,0,0,0.0927099) 71.72%, rgba(0,0,0,0.132754) 73.67%, rgba(0,0,0,0.177076) 75.61%, rgba(0,0,0,0.222924) 77.56%, rgba(0,0,0,0.267246) 79.5%, rgba(0,0,0,0.30729) 81.44%, rgba(0,0,0,0.341035) 83.39%, rgba(0,0,0,0.367336) 85.33%, rgba(0,0,0,0.385796) 87.28%, rgba(0,0,0,0.396541) 89.22%, rgba(0,0,0,0.4) 91.17%)",
-                }}
-              >
-                {/* The first box acts as a container that inherits style from the CardCover */}
-                <Box>
-                  <Box
-                    sx={{
-                      p: 2,
-                      display: "flex",
-                      alignItems: "center",
-                      gap: 1.5,
-                      flexGrow: 1,
-                      alignSelf: "flex-end",
-                    }}
-                  >
-                    <Typography level="h2" noWrap sx={{ fontSize: "lg" }}>
-                      <Link
-                        href="#dribbble-shot"
-                        overlay
-                        underline="none"
-                        sx={{
-                          color: "#fff",
-                          textOverflow: "ellipsis",
-                          overflow: "hidden",
-                          display: "block",
-                        }}
-                      >
-                        Yosemite
-                      </Link>
-                    </Typography>
-                  </Box>
-                </Box>
-              </CardCover>
-            </Box>
-          </Card>
+          <RechProjects />
         </Item>
       </Grid>
       <Grid item xs={3}>
         <Item>
-          <Card
-            sx={{
-              width: 300,
-              bgcolor: "initial",
-              boxShadow: "none",
-              "--Card-padding": "0px",
-            }}
-          >
-            <Box sx={{ position: "relative" }}>
-              <AspectRatio ratio="4/3">
-                <figure>
-                  <img
-                    src="https://images.unsplash.com/photo-1515825838458-f2a94b20105a?auto=format&fit=crop&w=300"
-                    srcSet="https://images.unsplash.com/photo-1515825838458-f2a94b20105a?auto=format&fit=crop&w=300&dpr=2 2x"
-                    loading="lazy"
-                    alt="Yosemite by Casey Horner"
-                  />
-                </figure>
-              </AspectRatio>
-              <CardCover
-                className="gradient-cover"
-                sx={{
-                  "&:hover, &:focus-within": {
-                    opacity: 1,
-                  },
-                  opacity: 0,
-                  transition: "0.1s ease-in",
-                  background:
-                    "linear-gradient(180deg, transparent 62%, rgba(0,0,0,0.00345888) 63.94%, rgba(0,0,0,0.014204) 65.89%, rgba(0,0,0,0.0326639) 67.83%, rgba(0,0,0,0.0589645) 69.78%, rgba(0,0,0,0.0927099) 71.72%, rgba(0,0,0,0.132754) 73.67%, rgba(0,0,0,0.177076) 75.61%, rgba(0,0,0,0.222924) 77.56%, rgba(0,0,0,0.267246) 79.5%, rgba(0,0,0,0.30729) 81.44%, rgba(0,0,0,0.341035) 83.39%, rgba(0,0,0,0.367336) 85.33%, rgba(0,0,0,0.385796) 87.28%, rgba(0,0,0,0.396541) 89.22%, rgba(0,0,0,0.4) 91.17%)",
-                }}
-              >
-                {/* The first box acts as a container that inherits style from the CardCover */}
-                <Box>
-                  <Box
-                    sx={{
-                      p: 2,
-                      display: "flex",
-                      alignItems: "center",
-                      gap: 1.5,
-                      flexGrow: 1,
-                      alignSelf: "flex-end",
-                    }}
-                  >
-                    <Typography level="h2" noWrap sx={{ fontSize: "lg" }}>
-                      <Link
-                        href="#dribbble-shot"
-                        overlay
-                        underline="none"
-                        sx={{
-                          color: "#fff",
-                          textOverflow: "ellipsis",
-                          overflow: "hidden",
-                          display: "block",
-                        }}
-                      >
-                        Yosemite
-                      </Link>
-                    </Typography>
-                  </Box>
-                </Box>
-              </CardCover>
-            </Box>
-          </Card>
+          <RusselProjects />
+        </Item>
+      </Grid>
+      <Grid item xs={3}>
+        <Item>
+          <TonProjects />
         </Item>
       </Grid>
     </React.Fragment>
