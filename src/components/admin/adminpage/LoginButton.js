ddb4815e68539a437bb477b8b1247678f5cc1c79
Jann Fraulien Madduma <madduma.jannfraulien@gmail.com>
2022-10-25 16:26:34 +0800

    admin design

--- src/components/admin/adminpage/LoginButton.js
+++ src/components/admin/adminpage/LoginButton.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import { Box } from "@mui/system";
 import {
+  AppBar,
   Avatar,
   Checkbox,
   Container,
@@ -14,6 +15,7 @@ import {
   Link,
   TextField,
   ThemeProvider,
+  Toolbar,
   Typography,
 } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
@@ -33,7 +35,7 @@ function Copyright(props) {
     >
       {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        condifyph
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
@@ -61,16 +63,6 @@ export default function SignInButton() {
   const handleDialogClose = () => {
     setOpen(false);
   };
-
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   console.log("called handleSubmit");
-  //   const data = new FormData(event.currentTarget);
-  //   console.log({
-  //     email: data.get("email"),
-  //     password: data.get("password"),
-  //   });
-  // };
 
   const [error, setError] = React.useState("");
   const dispatch = useDispatch();
@@ -100,117 +92,161 @@ export default function SignInButton() {
   };
 
   return (
-    <Box>
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
-      >
-        <Box>
-          <Typography variant="h1" component="h2">
-            CODIFY
-          </Typography>
-        </Box>
-        <Box>
-          <Button
-            variant="outlined"
-            sx={{ width: 200, padding: 1, margin: 2 }}
-            onClick={() => {
-              handleDialogOpen();
-            }}
+    <div>
+      <Box>
+        <Container sx={{ height: "100%" }}>
+          <Grid
+            container
+            spacing={2}
+            sx={{ height: "100%", alignContent: "center" }}
           >
-            Login
-          </Button>
-          <Typography>Client and Project Management page</Typography>
-        </Box>
-      </Box>
+            <Grid item xs={12}>
+              <Box
+                sx={{
+                  backgroundColor: "#56C6F4",
+                  height: "200px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography
+                  component="h3"
+                  variant="h3"
+                  align="center"
+                  color="white"
+                  sx={{
+                    fontFamily: "Poppins",
+                  }}
+                >
+                  <strong>codifyph</strong>
+                </Typography>
+              </Box>
+            </Grid>
 
+            <Grid item xs={12}>
+              <Box
+                sx={{
+                  height: "200px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                  paddingTop: "50px",
+                }}
+              >
+                <Button
+                  sx={{ width: 200 }}
+                  className="Button"
+                  variant="contained"
+                  size="large"
+                  style={{
+                    backgroundColor: "#C2E9F2",
+                    boxShadow: "-1px 1px 13px 0px rgba(194,233,242,1)",
+                    webkitBoxShadow: " -1px 1px 13px 0px rgba(194,233,242,1)",
+                    mozBoxShadow: "-1px 1px 13px 0px rgba(194,233,242,1)",
+                    color: "#404040",
+                  }}
+                  onClick={() => {
+                    handleDialogOpen();
+                  }}
+                >
+                  Login
+                </Button>
+                <Typography
+                  color="#404040"
+                  variant="h6"
+                  style={{
+                    marginTop: "10px",
+                    paddingTop: "50px",
+                  }}
+                >
+                  Client and Project Management page
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
       <Dialog open={open} onClose={handleDialogClose}>
         <DialogContent>
           <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-              <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 2,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
               <Box
                 sx={{
-                  marginTop: 8,
+                  marginTop: 3,
+                  marginBottom: 3,
                   display: "flex",
                   flexDirection: "column",
                   alignItems: "center",
                 }}
               >
-                <Box>
-                  <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                    <LockOutlinedIcon />
-                  </Avatar>
-                  <Typography component="h1" variant="h5">
-                    Sign in
-                  </Typography>
-                </Box>
-
-                <Box
-                  component="form"
-                  onSubmit={handleSubmit}
-                  noValidate
-                  sx={{ mt: 1 }}
+                <Typography component="h1" variant="h2" color="#56C6F4">
+                  Login
+                </Typography>
+                <Typography component="h1" variant="h4" color="#56C6F4">
+                  Sign in to continue
+                </Typography>
+              </Box>
+            </Box>
+            <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1 }}
+              >
+                {error && <span>{error}</span>}
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  onChange={(e) => onValueChange(e)}
+                  autoFocus
+                />
+                <TextField
+                  autoFocus
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={(e) => onValueChange(e)}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, backgroundColor: "#77C7D9" }}
                 >
-                  {error && <span>{error}</span>}
-                  <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    onChange={(e) => onValueChange(e)}
-                    autoFocus
-                  />
-                  <TextField
-                    autoFocus
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    onChange={(e) => onValueChange(e)}
-                  />
-                  <FormControlLabel
-                    control={<Checkbox value="remember" color="primary" />}
-                    label="Remember me"
-                  />
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                  >
-                    Sign In
-                  </Button>
-                  {/* <Grid container>
-                      <Grid item xs>
-                        <Link href="#" variant="body2">
-                          Forgot password?
-                        </Link>
-                      </Grid>
-                      <Grid item>
-                        <Link href="#" variant="body2">
-                          {"Don't have an account? Sign Up"}
-                        </Link>
-                      </Grid>
-                    </Grid> */}
-                </Box>
+                  Sign In
+                </Button>
               </Box>
               <Copyright sx={{ mt: 8, mb: 4 }} />
             </Container>
           </ThemeProvider>
         </DialogContent>
       </Dialog>
-    </Box>
+    </div>
   );
 }
