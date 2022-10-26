025254a502b4cfd56bdae210f4fbef3d3a62d140
SselJay <russeljaybuan@gmail.com>
2022-10-26 10:20:15 +0800

    commented dots

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -33,32 +33,32 @@ const theme = createTheme(themeInit, {
 
 const Home = ({}) => {
 
-  const stylesdot = {
-    paperContainer: {
-        height: 70,
-        backgroundImage: `url(${"../images/dots.png"})`,
-        width: 200,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        position: 'absolute',
-        marginTop: 5290,
-        marginLeft: 10,
-        transform: 'rotate(90deg)'
-    }
-};
-  const stylesdottwo = {
-    paperContainer: {
-        height: 70,
-        backgroundImage: `url(${"../images/dots.png"})`,
-        width: 200,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        position: 'absolute',
-        marginTop: 5750,
-        marginLeft: 1270,
-        transform: 'rotate(90deg)'
-    }
-};
+//   const stylesdot = {
+//     paperContainer: {
+//         height: 70,
+//         backgroundImage: `url(${"../images/dots.png"})`,
+//         width: 200,
+//         backgroundSize: 'cover',
+//         backgroundPosition: 'center',
+//         position: 'absolute',
+//         marginTop: 5290,
+//         marginLeft: 10,
+//         transform: 'rotate(90deg)'
+//     }
+// };
+//   const stylesdottwo = {
+//     paperContainer: {
+//         height: 70,
+//         backgroundImage: `url(${"../images/dots.png"})`,
+//         width: 200,
+//         backgroundSize: 'cover',
+//         backgroundPosition: 'center',
+//         position: 'absolute',
+//         marginTop: 5750,
+//         marginLeft: 1270,
+//         transform: 'rotate(90deg)'
+//     }
+// };
 
   return (
     <ThemeProvider theme={theme}>
@@ -80,12 +80,12 @@ const Home = ({}) => {
         <Footer />
       </div>
 
-      <div style={stylesdot.paperContainer}>
+      {/* <div style={stylesdot.paperContainer}>
 
               </div>
       <div style={stylesdottwo.paperContainer}>
 
-              </div>
+              </div> */}
     </ThemeProvider>
   );
 };
